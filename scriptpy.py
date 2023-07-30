c5194a1d784e042b29b078e2f973ea575f23d257
VINISH <140247276+codevin-projects@users.noreply.github.com>
2023-07-30 22:08:37 +0530

    scriptpy.py

--- scriptpy.py
+++ scriptpy.py
@@ -3,13 +3,13 @@ def voiceChangeFemale():
     eng = pyttsx3.init()
     voice = eng.getProperty('voices') 
     eng.setProperty('voice', voice[1].id)
-    cmd = "This is a demonstration of how to convert index of voice using pyttsx3 library in python."
+    cmd = input("Enter text here: ")
     eng.say(cmd)
     eng.runAndWait()
 
 def voiceChangeMale():
     tesp = pyttsx3.init()
-    res = input("Enter the text: ")
+    res = input("Enter text here: ")
     tesp.say(res)
     tesp.runAndWait()
 
